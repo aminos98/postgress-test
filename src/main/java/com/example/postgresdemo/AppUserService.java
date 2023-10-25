6d075afe11324de58a72ee5573f94f8692342e70
HamzaGbada <gbadahamza18@gmail.com>
2023-10-25 15:55:47 +0100

    service

---
+++ src/main/java/com/example/postgresdemo/AppUserService.java
@@ -0,0 +1,20 @@
+package com.example.postgresdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    public AppUser findByUsername(String username) {
+        return appUserRepository.findByUsername(username);
+    }
+
+    public AppUser save(AppUser user) {
+        return appUserRepository.save(user);
+    }
+
+}
+
